d0ff8d058960f95cbbfe64a788e417d9b75e2079
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2022-02-24 20:55:02 -0700

    changed location for RangeTest

--- JFreeChart_Lab3/src/org/jfree/data/RangeTest.java
+++ JFreeChart_Lab3/src/org/jfree/data/RangeTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class RangeTest(){
+public class RangeTest{
 
     private double upper;
     private double lower; 
